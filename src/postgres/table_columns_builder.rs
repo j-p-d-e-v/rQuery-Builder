560cb9ce72a5a6f693675bd45e7d1c238c7cdadc
JP <johmateo@cisco.com>
2025-07-15 02:16:51 +0800

    Fixed the value enclosing it with ''

--- src/postgres/table_columns_builder.rs
+++ src/postgres/table_columns_builder.rs
@@ -3,6 +3,6 @@ pub struct TableColumnsBuilder;
 
 impl TableColumnsBuilder {
     pub fn new(table: &str) -> String {
-        format!("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = {table}")
+        format!("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '{table}'")
     }    
 }
