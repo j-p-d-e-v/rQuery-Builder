1900f0a00296c277527791e01445fda5fe498134
JP <jp@tiktoklive.com>
2025-07-15 23:40:00 +0800

    Changed new to build in Table Columns Builder

--- src/postgres/table_columns_builder.rs
+++ src/postgres/table_columns_builder.rs
@@ -1,8 +1,8 @@
-#[derive(Debug,Clone)]
+#[derive(Debug, Clone)]
 pub struct TableColumnsBuilder;
 
 impl TableColumnsBuilder {
-    pub fn new(table: &str) -> String {
+    pub fn build(table: &str) -> String {
         format!("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '{table}'")
-    }    
+    }
 }
